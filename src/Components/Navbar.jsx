31ddbc63f2919faba9c08c8238cca6aed391a0c2
Alpay Oğuzhan Palabıyık <alpayoguz97@gmail.com>
2022-06-11 12:56:49 +0300

    add css animations to heroes page

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,12 +8,22 @@ const Navbar = () => {
       <div className="navbar__content">
         <a className="logo_img"></a>
         <ul className="navbar__items">
-          <li className="navbar__item">HOME</li>
-          <Link to={`/heroes`}>
-            <li className="navbar__item">HEROES</li>
-          </Link>
-
-          <li className="navbar__item">SEARCH PLAYER</li>
+          <li>
+            <Link className="navbar__item" to={`/`}>
+              HOME
+            </Link>
+          </li>
+          <li>
+            {" "}
+            <Link className="navbar__item" to={`/heroes`}>
+              HEROES
+            </Link>
+          </li>
+          <li>
+            <Link className="navbar__item" to={`/heroes`}>
+              SEARCH PLAYER
+            </Link>
+          </li>
         </ul>
       </div>
     </div>
